8c9b80b265295f9b030eae9a1585a423f9ff65b0
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-24 12:12:32 -0800

    color picking, color display, rgb value display now functional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from './components/Header/Header';
+import ColorDisplay from './components/ColorDisplay/ColorDisplay';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Switch>
+          <Route path="/:red/:green/:blue">
+            <ColorDisplay />
+          </Route>
+          <Route path="*">
+            <Redirect to="/0/0/0" />
+          </Route>
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="App-header">
-      <NavLink to="/uhhhh">uhhhh</NavLink>
+      <NavLink to="/237/92/56">hot dog</NavLink>
+      <NavLink to="/247/175/234">cotton candy pink</NavLink>
+      <NavLink to="/64/102/84">seafloor</NavLink>
     </header>
   );
 }
