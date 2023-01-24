9de17b4571e70c17bc6711a3870d08b38c8a4a1e
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-23 18:08:25 -0800

    writing basic components and such

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <NavLink to="/uhhhh">uhhhh</NavLink>
+    </header>
+  );
+}
